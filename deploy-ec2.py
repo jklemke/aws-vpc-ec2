5e636afd1607ca60f7e30cf9ec194f4bbdb05259
jklemke <jklemke@grox.com>
2021-07-14 17:54:00 +0100

    Delete deploy-ec2.py

--- deploy-ec2.py
+++
@@ -1,3 +0,0 @@
-aws cloudformation deploy --template-file .\grox-ec2-dev-101.yaml --stack-name teal-dev
-aws cloudformation deploy --template-file .\grox-ec2-dmz-101.yaml --stack-name teal-dmz
-aws cloudformation deploy --template-file .\grox-ec2-prv-101.yaml --stack-name teal-prv
